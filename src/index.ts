2fb0d65e0906a75cd3f153859bf7dced4045c3c5
Zoltan Kochan <z@kochan.io>
2018-03-11 21:10:29 +0200

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,90 @@
+import fs = require('fs')
+import loadYamlFile = require('load-yaml-file')
+import path = require('path')
+import exists = require('path-exists')
+// TODO: move to separate packaeg
+import {Modules, read as readModules} from 'supi/lib/fs/modulesController'
+import {Test} from 'tape'
+import writePkg = require('write-pkg')
+import isExecutable from './isExecutable'
+
+export default (t: Test, projectPath: string, encodedRegistryName?: string) => {
+  const ern = encodedRegistryName || 'localhost+4873'
+  const modules = path.join(projectPath, 'node_modules')
+  let cachedStorePath: string
+  const project = {
+    requireModule (pkgName: string) {
+      return require(path.join(modules, pkgName))
+    },
+    async has (pkgName: string) {
+      t.ok(await exists(path.join(modules, pkgName)), `${pkgName} is in node_modules`)
+    },
+    async hasNot (pkgName: string) {
+      t.notOk(await exists(path.join(modules, pkgName)), `${pkgName} is not in node_modules`)
+    },
+    async getStorePath () {
+      if (!cachedStorePath) {
+        const modulesYaml = await readModules(modules)
+        if (!modulesYaml) {
+          throw new Error('Cannot find module store')
+        }
+        cachedStorePath = modulesYaml.store
+      }
+      return cachedStorePath
+    },
+    async resolve (pkgName: string, version?: string, relativePath?: string) {
+      const pkgFolder = version ? path.join(ern, pkgName, version) : pkgName
+      if (relativePath) {
+        return path.join(await project.getStorePath(), pkgFolder, 'package', relativePath)
+      }
+      return path.join(await project.getStorePath(), pkgFolder, 'package')
+    },
+    async storeHas (pkgName: string, version?: string) {
+      const pathToCheck = await project.resolve(pkgName, version)
+      t.ok(await exists(pathToCheck), `${pkgName}@${version} is in store (at ${pathToCheck})`)
+    },
+    async storeHasNot (pkgName: string, version?: string) {
+      try {
+        const pathToCheck = await project.resolve(pkgName, version)
+        t.notOk(await exists(pathToCheck), `${pkgName}@${version} is not in store (at ${pathToCheck})`)
+      } catch (err) {
+        if (err.message === 'Cannot find module store') {
+          t.pass(`${pkgName}@${version} is not in store`)
+          return
+        }
+        throw err
+      }
+    },
+    isExecutable (pathToExe: string) {
+      return isExecutable(t, path.join(modules, pathToExe))
+    },
+    async loadCurrentShrinkwrap () {
+      try {
+        return await loadYamlFile<any>('node_modules/.shrinkwrap.yaml') // tslint:disable-line
+      } catch (err) {
+        if (err.code === 'ENOENT') return null
+        throw err
+      }
+    },
+    async loadModules () {
+      try {
+        return await loadYamlFile<any>('node_modules/.modules.yaml') // tslint:disable-line
+      } catch (err) {
+        if (err.code === 'ENOENT') return null
+        throw err
+      }
+    },
+    async loadShrinkwrap () {
+      try {
+        return await loadYamlFile<any>('shrinkwrap.yaml') // tslint:disable-line
+      } catch (err) {
+        if (err.code === 'ENOENT') return null
+        throw err
+      }
+    },
+    async writePackageJson (pkgJson: object) {
+      await writePkg(projectPath, pkgJson)
+    },
+  }
+  return project
+}
